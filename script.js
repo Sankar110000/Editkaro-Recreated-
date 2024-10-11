c2ab82507ecf4fdaf2989dfa5d63567a4b2627fe
sankar110000 <skaibalya748@gmail.com>
2024-10-11 12:32:04 -0700

    some chnages are done

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 let lastScrollPostion = 0;
 let navbar = document.querySelector(".navbar");
+let vdoPreviews = document.querySelectorAll(".videoContainer video");
+let scrollHeading2 = document.querySelector("#scrollHeading2");
 
 window.addEventListener("scroll", () => {
     let currScrollPosition = window.scrollY;
-    if(currScrollPosition > 550){
+    if(currScrollPosition > 500){
         navbar.style.backgroundColor = "#D8D2C2";
-        console.dir(navbar.style.backgroundColor)
     }
     if(currScrollPosition < 550){
         navbar.style.backgroundColor = "transparent";
-        console.dir(navbar.style.backgroundColor)
     }
 });
+
+vdoPreviews.forEach(video => {
+    video.addEventListener("mouseenter", () => {
+        setTimeout(() => {
+            video.play();
+        }, 300);
+    })
+    video.addEventListener("mouseleave", () => {
+        setTimeout(() => {
+            video.pause();
+        }, 300);;
+    })
+});
+
+scrollHeading2.addEventListener("click", () => {
+    window.scrollTo({
+        top: 1010,
+        left: 0,
+        behavior: "smooth"
+    })
+})
